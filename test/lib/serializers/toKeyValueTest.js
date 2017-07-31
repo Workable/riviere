3f6908474008325c79b90476906ea01da2d16a18
matzavinos <matzavinos@workable.com>
2017-07-31 16:12:16 +0300

    tests: remove console.log line

--- test/lib/serializers/toKeyValueTest.js
+++ test/lib/serializers/toKeyValueTest.js
@@ -22,7 +22,6 @@ test('should serialize an object', t => {
     };
 
     const res = serialize(input);
-    console.log('r:', res);
     t.is(res, 'testStr="testStrOk", testArr=[testItem1="testItem1Ok", testItem2="testItem2Ok"], testBool=true, testBoolFalse=false, testSymbol="Symbol(test)", testDate=Mon Jul 31 2017 14:58:41 GMT+0300 (EEST), testNum=1');
     t.pass();
 });
