25fbe365e6e65ef46b885b89127098c0567e2207
matzavinos <matzavinos@workable.com>
2017-10-20 10:17:58 +0300

    update example

--- examples/hello_with_outgoing_traffic.js
+++ examples/hello_with_outgoing_traffic.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 const Riviere = require('riviere');
+
+// this is just an example, you can use any http library
 const rp = require('request-promise');
 
 const app = new Koa();
@@ -8,8 +10,12 @@ app.use(Riviere.middleware());
 app.use(async function(ctx) {
   await rp({
     uri: 'https://www.google.com',
+    // You can include the X-Riviere-Id header
+    // to trace the request's context inside which
+    // the external request is made
+    // This is optional but recommended for better tracing:
     headers: {
-      'X-Riviere-Id': ctx.request.headers['x-riviere-id']
+      'X-Riviere-Id': ctx.request.headers['x-riviere-id'] // notice that this s lowercase
     }
   });
   ctx.body = 'Hello World';
