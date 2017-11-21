bb60da3c9725bd1aad4a147783c4b7a2e7ebc298
Alkis Kalogeris <alkis.kalogeris@gmail.com>
2017-11-21 16:04:57 +0200

    add types

---
+++ types/riviere.d.ts
@@ -0,0 +1,24 @@
+export function middleware(options: {
+  color?: boolean,
+  adapter?: any,
+  context?: (ctx: any) => any,
+  errors?: {
+    callback: (ctx: any, error: Error) => any
+  },
+  health?: Array<{path: string; method: string; }>,
+  logger?: {
+    info: any,
+    error: any
+  },
+  inbound?: {
+    level: string
+  },
+  outbound?: {
+    enabled: boolean,
+    level: string
+  },
+  bodyKeys?: Array<string>,
+  headersRegex?: RegExp,
+  traceHeaderName?: string,
+  forceIds?: boolean
+}): any;
