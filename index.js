22264a154519ce14fb6034e388ee0430c0e27a2c
Dimitris Klouvas <klouvas@workable.com>
2019-06-24 19:10:02 +0300

    Add `headersRegex` support in outbound_request.
    Requirements:
    1. Each outbound request should contain inbound request_id
    2. Each outbound request should have it's own request_id, but we should be able to manually override
    
    Solution:
    - Add `headersRegex` support in outbound request to log matching headers (requirement 1)
    - Pass header matching `headersRegex` to outbound request to log inbound request in outbound (requirement 1)
    - Set `x-riviere-id` header of  outbound request to explicitly set the request id(requirement 2)
    
    **Important**
    - Headers are accessed with lowercase format
    - Set  header matching `headersRegex` in outbound requests to log inbound request as part of outbound

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ const httpsProxy = require('./lib/proxies/https').proxy;
 const { EVENT } = Loggable;
 
 function buildRiviere(options = {}) {
-  const { errors = {}, logger, inbound, outbound, traceHeaderName } = defaultsDeep(options, defaultOptions(options));
+  const { errors = {}, logger, inbound, outbound, traceHeaderName, headersRegex } = defaultsDeep(
+    options,
+    defaultOptions(options)
+  );
 
   const loggable = new Loggable(options);
 
   if (outbound.enabled) {
-    const handler = options.adapter.requestProxy({ level: outbound.level, logger, traceHeaderName, opts: outbound });
+    const handler = options.adapter.requestProxy({
+      level: outbound.level,
+      logger,
+      traceHeaderName,
+      opts: {
+        headersRegex,
+        ...outbound
+      }
+    });
     httpProxy(handler);
 
     if (outbound.https) {
--- test/lib/transformers/mapOutReqTest.js
+++ test/lib/transformers/mapOutReqTest.js
@@ -23,4 +23,34 @@ describe('mapOutReq', () => {
       log_tag: 'outbound_request'
     });
   });
+
+  it('should able to get matching headers', () => {
+    const inMsg = {
+      uri: {
+        protocol: 'http:',
+        hostname: 'hostname',
+        path: 'path',
+        query: 'query'
+      },
+      headers: {
+        'x-transaction-id': 'transaction-id',
+        aloha: 'asdasdfasdf'
+      }
+    };
+    const result = mapOutReq(inMsg, undefined, {
+      headersRegex: new RegExp('^X-.*', 'i')
+    });
+    result.should.eql({
+      method: undefined,
+      protocol: 'http',
+      host: 'hostname',
+      port: undefined,
+      path: 'path',
+      query: 'query',
+      href: undefined,
+      requestId: undefined,
+      log_tag: 'outbound_request',
+      'headers.x-transaction-id': 'transaction-id'
+    });
+  });
 });
