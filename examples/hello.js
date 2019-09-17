35383606761e3d5e8a985dfa74ac033381f4eca4
Manos Kouvarakis <manoskouvarakis@gmail.com>
2019-09-17 20:31:10 +0300

    Update examples/hello.js
    Co-Authored-By: Nikos Kostoulas <nikostoulas@users.noreply.github.com>

--- examples/hello.js
+++ examples/hello.js
@@ -3,7 +3,8 @@ const riviere = require('../index');
 
 const app = new Koa();
 
-app.use(riviere({ styles: ['extended-11', 'json'] }));
+app.use(riviere({ styles: ['extended', 'json'] })); 
+``` ??
 
 app.use(function(ctx) {
   ctx.body = 'Hello World';
